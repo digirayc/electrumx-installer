349a00f243b8e3c58413f2a6cfb80456d107392c
MadCatMining <zuklys@mail.ru>
2022-02-07 12:15:32 +0200

    Update RocksDB to v6.8.2
    Update RocksDB to currently latest v6.8.2

--- distributions/base-compile-rocksdb.sh
+++ distributions/base-compile-rocksdb.sh
@@ -9,7 +9,7 @@ function install_rocksdb {
 	git clone https://github.com/facebook/rocksdb.git /tmp/rocksdb
 	cd /tmp/rocksdb
 	# Of course pyrocksdb wouldn't be pyrocksdb if it supported the latest version
-	git checkout v4.5.1
+	git checkout v6.8.2
 	_info "Compiling RocksDB... This will take a while."
 	make shared_lib -j 2 || _error "Could not compile rocksdb" 1
 	make install-shared INSTALL_PATH=/usr || _error "Could not install rocksdb" 1
